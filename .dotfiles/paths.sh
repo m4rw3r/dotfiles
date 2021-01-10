b9f73096fb4c66f4a17cb59d5c4e7f42fcbb6ccc
Martin Wernstål <m4rw3r@gmail.com>
2021-01-10 20:50:15 +0100

    fix: duplicate .cargo/bin paths

--- .dotfiles/paths.sh
+++ .dotfiles/paths.sh
@@ -10,7 +10,6 @@ export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/sbin:$PATH"
 
 # Rustup
 [[ -f $HOME/.cargo/env ]] && source $HOME/.cargo/env
-export PATH="$HOME/.cargo/bin:$PATH"
 
 # Haskell binaries from cabal
 [[ -d $HOME/Library/Haskell/bin ]] && export PATH="$HOME/Library/Haskell/bin:$PATH"

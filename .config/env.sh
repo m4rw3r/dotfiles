174a007e67458e3fea240c527f92da829d1cc3ee
Martin Wernstål <m4rw3r@gmail.com>
2022-06-25 12:37:05 +0200

    feat: XDG support for NPM

--- .config/env.sh
+++ .config/env.sh
@@ -2,10 +2,20 @@
 
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
+
+# XDG
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-"$HOME/.cache"}"
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
+
+# Zsh
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
 export MODULES_DIR="$ZDOTDIR/plugins"
+
+# NPM XDG support
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/config"
+export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
+#export NPM_CONFIG_TMP="$XDG_RUNTIME_DIR/npm"
 
 export VISUAL='vim'
 # Use this command in psql to enable less pager: \pset pager always

e9ef2112bb3a6ab0718dff1cea4ee176e8069b13
Martin Wernstål <m4rw3r@gmail.com>
2022-08-09 17:59:12 +0200

    feat(env): XDG_STATE_HOME

--- .config/env.sh
+++ .config/env.sh
@@ -8,6 +8,7 @@ export XDG_CACHE_HOME="${XDG_CACHE_HOME:-"$HOME/.cache"}"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
 export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-"/var/run/users/$UID"}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-"$HOME/.local/state"}"
 
 # Zsh
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"

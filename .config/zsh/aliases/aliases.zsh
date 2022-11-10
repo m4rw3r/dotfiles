d5b36624c61fc6c3d96f57e61216ea53d1ff7021
Martin Wernstål <m4rw3r@gmail.com>
2022-11-10 12:10:30 +0100

    feat(zsh): ssh is normal ssh, s is Kitty ssh

--- .config/zsh/aliases/aliases.zsh
+++ .config/zsh/aliases/aliases.zsh
@@ -38,7 +38,7 @@ fi
 if test -n "$KITTY_INSTALLATION_DIR"; then
 	alias icat="kitty +kitten icat"
 	# Kitty terminfo does not work well on older linux-servers for some reason, fallback to xterm-256color
-	alias ssh="TERM=xterm-256color kitty +kitten ssh"
+	alias s="TERM=xterm-256color kitty +kitten ssh"
 	alias kdiff="kitty +kitten diff"
 fi
 

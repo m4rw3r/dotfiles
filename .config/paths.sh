1bbe84177996bfef91bf9343bcb6d7b1745443bb
Martin Wernstål <m4rw3r@gmail.com>
2023-05-04 16:09:04 +0200

    fix(env): reordered paths to add ~/.local/bin first

--- .config/paths.sh
+++ .config/paths.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# OS X
+if [[ -d /usr/local/sbin ]]; then
+	export PATH="/usr/local/sbin:$PATH"
+fi
+## MacPorts
+if [[ -d /opt/local/bin ]]; then
+	export PATH="/opt/local/bin:$PATH"
+fi
+if [[ -d /opt/local/sbin ]]; then
+	export PATH="/opt/local/sbin:$PATH"
+fi
+## X11
+if [[ -d /opt/X11/bin ]]; then
+	export PATH="/opt/X11/bin:$PATH"
+fi
+## MySQL
+if [[ -d /usr/local/mysql/bin ]]; then
+	export PATH="/usr/local/mysql/bin:$PATH"
+fi
+
 # Rustup
 if [[ -f $HOME/.cargo/env ]]; then
 	source $HOME/.cargo/env
@@ -7,27 +27,4 @@ fi
 # Python PIP and other user stuff
 if [[ -d $HOME/.local/bin ]]; then
 	export PATH="$HOME/.local/bin:$PATH"
-fi
-
-# OS X
-
-if [[ -d /usr/local/sbin ]]; then
-	export PATH="/usr/local/sbin:$PATH"
-fi
-
-# MacPorts
-if [[ -d /opt/local/bin ]]; then
-	export PATH="/opt/local/bin:$PATH"
-fi
-if [[ -d /opt/local/sbin ]]; then
-	export PATH="/opt/local/sbin:$PATH"
-fi
-
-# X11
-if [[ -d /opt/X11/bin ]]; then
-	export PATH="/opt/X11/bin:$PATH"
-fi
-# MySQL
-if [[ -d /usr/local/mysql/bin ]]; then
-	export PATH="/usr/local/mysql/bin:$PATH"
 fi

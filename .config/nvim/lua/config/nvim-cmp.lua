8ec17498d872c7db00a58687aff40dbb919cbe3f
Martin Wernstål <m4rw3r@gmail.com>
2025-02-04 17:21:32 +0100

    feat(nvim): wip of better nvim cmp

--- .config/nvim/lua/config/nvim-cmp.lua
+++ .config/nvim/lua/config/nvim-cmp.lua
@@ -13,6 +13,7 @@ function M.config()
   unpack = unpack or table.unpack
 
   local cmp = require("cmp")
+  local luasnip = require("luasnip")
   local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 
@@ -24,29 +25,104 @@ function M.config()
       completion = cmp.config.window.bordered(),
       documentation = cmp.config.window.bordered(),
     },
+    completion = {
+      -- autocomplete = false,
+      completeopt = "menuone,preview,noselect",
+    },
+    -- Required along with noselect to not have cmp select but not insert the option on either "words<Tab>" or <C-n>
+    preselect = cmp.PreselectMode.None,
     mapping = cmp.mapping.preset.insert({
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+
+--      ["<Tab>"] = cmp.mapping(function(fallback)
+--        if cmp.visible() then
+--          cmp.select_next_item()
+--        elseif luasnip.locally_jumpable(1) then
+--          luasnip.jump(1)
+--        else
+--          fallback()
+--        end
+--      end, { "i", "s" }),
+--      ["<S-Tab>"] = cmp.mapping(function(fallback)
+--        if cmp.visible() then
+--          cmp.select_prev_item()
+--        elseif luasnip.locally_jumpable(-1) then
+--          luasnip.jump(-1)
+--        else
+--          fallback()
+--        end
+--      end, { "i", "s" }),
+--      ['<CR>'] = cmp.mapping(function(fallback)
+--        if cmp.visible() then
+--          if luasnip.expandable() then
+--            luasnip.expand()
+--          else
+--            cmp.confirm({ select = true })
+--          end
+--        else
+--          fallback()
+--        end
+--      end),
+
       ["<Tab>"] = function(fallback)
-        if not cmp.select_next_item() then
-          if vim.bo.buftype ~= "prompt" and has_words_before() then
-            cmp.complete()
+        if cmp.visible() then
+          if #cmp.get_entries() == 1 then
+            cmp.confirm({ select = true })
           else
-            fallback()
+            cmp.select_next_item()
           end
+        elseif has_words_before() then
+          cmp.complete()
+
+          if #cmp.get_entries() == 1 then
+            cmp.confirm({ select = true })
+          else
+            -- "autoselect" replacement
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+          end
+        else
+          fallback()
         end
+
+        -- if not cmp.select_next_item() then
+        --   if vim.bo.buftype ~= "prompt" and has_words_before() then
+        --     cmp.complete()
+        --   else
+        --     fallback()
+        --   end
+        -- end
       end,
       ["<S-Tab>"] = function(fallback)
-        if not cmp.select_prev_item() then
-          if vim.bo.buftype ~= "prompt" and has_words_before() then
-            cmp.complete()
+        if cmp.visible() then
+          if #cmp.get_entries() == 1 then
+            cmp.confirm({ select = true })
           else
-            fallback()
+            cmp.select_prev_item()
           end
+        elseif has_words_before() then
+          cmp.complete()
+
+          if #cmp.get_entries() == 1 then
+            cmp.confirm({ select = true })
+          else
+            -- "autoselect" replacement
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+          end
+        else
+          fallback()
         end
+
+        -- if not cmp.select_prev_item() then
+        --   if vim.bo.buftype ~= "prompt" and has_words_before() then
+        --     cmp.complete()
+        --   else
+        --     fallback()
+        --   end
+        -- end
       end,
-      ["<C-e>"] = cmp.mapping.abort(),
       -- Safe selection on enter only
       -- when something has explicitly been selected
       ["<CR>"] = cmp.mapping({
@@ -69,7 +145,7 @@ function M.config()
     }),
     snippet = {
       expand = function(args)
-        require("luasnip").lsp_expand(args.body)
+        luasnip.lsp_expand(args.body)
       end,
     },
   })

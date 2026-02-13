b312c38e74126f04d720239bac45ba406458b3b3
Martin Wernstål <m4rw3r@gmail.com>
2026-02-13 10:02:09 +0100

    feat(env): XDG_BIN_DIR

--- .config/env.sh
+++ .config/env.sh
@@ -9,6 +9,7 @@ export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
 export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-"/var/run/users/$UID"}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-"$HOME/.local/state"}"
+export XDG_BIN_DIR="${XDG_BIN_DIR:-"$HOME/.local/bin"}"
 
 # Zsh
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
